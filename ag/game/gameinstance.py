723d663e30571c0cc36d895eda440bfd9c2be152
sschaeffer <sschaeffer@hotmail.com>
2021-10-16 13:35:11 +0900

    afternoon commit

--- ag/game/gameinstance.py
+++ ag/game/gameinstance.py
@@ -3,7 +3,8 @@ from sys import path
 path.append("/home/integ/Code/aghud")
 
 from ag.common.aghudconstants import AGHUDConstants
-import subprocess
+from ag.common.aghudconfig import AGHUDConfig
+from ag.game.gamequery import GameQuery
 
 #from ag.game.gamequery import GameQuery
 #from bc.game.bclevelfile import BCLevelFile
@@ -13,13 +14,15 @@ import subprocess
 
 from time import sleep, time
 from subprocess import run 
+from pathlib import Path
 
 class GameInstance():
 
     def __init__(self, minecraftdir, worldname, singleplayer,
                  autobackup=AGHUDConstants.AUTO_BACKUP_DEFAULT,
                  autobackupdelay=AGHUDConstants.AUTO_BACKUP_DELAY_DEFAULT,
-                 autoupdate=True, autoupdatedelay=1.0):
+                 autoupdate=AGHUDConstants.AUTO_UPDATE_DEFAULT,
+                 autoupdatedelay=AGHUDConstants.AUTO_UPDATE_DELAY_DEFAULT):
         """
         Parameters
         ----------
@@ -29,12 +32,129 @@ class GameInstance():
             The name of the current minecraft world
         ...
         """
-        self._minecraftdir=minecraftdir
-        self._worldname=worldname
-        self._singleplayer=singleplayer
-        self._autobackup=autobackup
-        self._autobackupdelay=autobackupdelay
-        self._autoupdate=autoupdate
-        self._autoupdatedelay=autoupdatedelay
+        self.set_minecraftdir(minecraftdir)
+        self.set_worldname(worldname)
+        self.set_servername(worldname)
+        self.set_singleplayer(singleplayer)
+
+        self._game_query = None
+        self._game_port = 0
+        self._game_status = AGHUDConstants.GAME_STARTING
+
+        self.set_autobackup(autobackup)
+        self.set_autobackupdelay(autobackupdelay)
+        self.set_autoupdate(autoupdate)
+        self.set_autoupdatedelay(autoupdatedelay)
+
+        self._last_update = 0.0
+        self._last_backup = 0.0
+
+    def set_minecraftdir(self, minecraftdir):
+        self._minecraftdir = minecraftdir
+
+    def set_worldname(self, worldname):
+        self._worldname = worldname
+
+    def set_servername(self, servername):
+        self._servername = servername
+
+    def set_singleplayer(self, singleplayer):
+        self._singleplayer = singleplayer
+
+    def set_autoupdate(self, autoupdate):
+        self._autoupdate = autoupdate
+
+    def set_autobackup(self, autobackup):
+        self._autobackup = autobackup
+
+    def set_autoupdatedelay(self, autoupdatedelay):
+        self._autoupdatedelay = autoupdatedelay
+
+    def set_autobackupdelay(self, autobackupdelay):
+        self._autobackupdelay = autobackupdelay
 
 
+    def update_ready(self):
+        if(self._autoupdate):
+           if time() > self._last_update + self._autoupdatedelay:
+               return True
+        return False
+
+    def backup_ready(self):
+        if(self._autobackup):
+           if time() > self._last_backup + self._autobackupdelay:
+               return True
+        return False
+
+    def update(self):
+        self.update_game_status()
+        self._last_update = time()
+
+    def backup(self):
+        self._last_backup = time()
+
+ 
+    def update_game_status(self):
+        """
+        if not singleplayer
+            query status via network
+        """
+        if(Path(f"{self._minecraftdir}/saves/{self._worldname}").is_dir()):
+            print(f"{self._minecraftdir}/saves/{self._worldname} is a directory")
+            levelfile_path=Path(f"{self._minecraftdir}/saves/{self._worldname}/level.dat")
+            if(levelfile_path.is_file()):
+                print(f"{levelfile_path} is a file")
+                if self._singleplayer:
+                    self._game_status = AGHUDConstants.GAME_RUNNING
+                else:
+                    if self._game_port == 0:
+                        serverproperties_path=Path(f"{self._minecraftdir}/server/{self._servername}/server.properties")
+                        if serverproperties_path.is_file():
+                            with open(serverproperties_path) as f:
+                                line  = f.readline()
+                                while line and self._game_port == 0:
+                                    if line[0] != '#':
+                                        key, value = line.split('=')
+                                        print(f"Key {key} Value {value}")
+                                        if key == "query.port":
+                                            print(f"Value {value}")
+                                            self._game_port = int(value)
+                                    line  = f.readline()
+                    if self._game_port != 0:
+                        self._game_query = GameQuery("localhost", self._game_port)
+
+
+            else:
+                if self._game_status !=  AGHUDConstants.GAME_STARTING: self._game_status = AGHUDConstants.GAME_RESET
+        else:
+            print(f"{self._minecraftdir}/saves/{self._worldname} is not a directory")
+            if self._game_status !=  AGHUDConstants.GAME_STARTING: self._game_status = AGHUDConstants.GAME_RESET
+
+# ----
+# UNIT TESTING ROUTINES - REMOVE BEFORE DEPLOYING RELEASE
+# ----
+def main(minecraftdir, worldname, singleplayer, autobackup, autobackupdelay):
+
+    print()
+    print("Game Instance:    Unit Testing")
+
+    game = GameInstance(minecraftdir,worldname,singleplayer,autobackup,autobackupdelay)
+    game.set_autoupdate(True)
+    game.set_autoupdatedelay(2.0)
+    game.set_autobackup(True)
+    game.set_autobackupdelay(5.0)
+
+    while(True):
+        print("loop")
+        if game.update_ready():
+            print("Update")
+            game.update()
+        if game.backup_ready():
+            print("Backup")
+            game.backup()
+        sleep(.4)
+
+
+if __name__ == "__main__":
+    (minecraftdir, worldname, singleplayer, autobackup, autobackupdelay) = AGHUDConfig.init_aghud("/home/integ/Code/aghud/config.json")
+    main(minecraftdir, worldname, singleplayer, autobackup, autobackupdelay)
---
+++ ag/scripts/backup-world.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+minecraftdir=~/.minecraft
+worldname='New\ World'
+
+MINECRAFTDIR=${1:-$minecraftdir}
+WORLDNAME=${2:-$worldname}
+
+BACKUPDATE=`date +'%Y-%m-%d_%H-%M-%S'`
+cd ${MINECRAFTDIR}/saves
+
+eval /usr/bin/zip -r "${MINECRAFTDIR}/backups/${WORLDNAME}_${BACKUPDATE}.zip" "./${WORLDNAME}"
